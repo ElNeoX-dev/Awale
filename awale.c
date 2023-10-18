1dcbafd60f666db53a569371e42632b6162ec66b
TheMoi <35933570+TheMoi@users.noreply.github.com>
2023-10-18 10:51:24 +0200

    Etape 2 Tim

--- awale.c
+++ awale.c
@@ -3,13 +3,15 @@
 #include <stdio.h>
 
 void afficherPlateau(int *plateau, char **joueur);
-void jouer(int *plateau, char **joueur, int j);
+void jouer(int *plateau, char **joueur, int *points, int j);
+int aFiniTerrainAdverse(int j, int *joueur, int CaseChoisie);
 int hasWin();
 
 int main(int argc, char **argv)
 {
 
     int *plateau = malloc(12 * sizeof(int));
+    int *points = malloc(2 * sizeof(int));
     int i;
 
     char *joueur[2] = {"Tim", "Hugo"};
@@ -17,7 +19,7 @@ int main(int argc, char **argv)
     // initialisation du plateau
     for (i = 0; i < 12; i++)
     {
-        plateau[i] = 4;
+        plateau[i] = 1;
     }
 
     // affichage du plateau
@@ -27,45 +29,106 @@ int main(int argc, char **argv)
     while (hasWin() == 0)
     {
         // joueur 1
-        jouer(plateau, joueur, 0);
+        jouer(plateau, joueur, points, 0);
         afficherPlateau(plateau, joueur);
 
         // joueur 2
-        jouer(plateau, joueur, 1);
+        jouer(plateau, joueur, points, 1);
         afficherPlateau(plateau, joueur);
     }
 }
 
-void jouer(int *plateau, char **joueur, int j)
+void jouer(int *plateau, char **joueur, int *points, int j)
 {
     int caseChoisie = -1;
+
     printf("************************************ \r\n");
 
-    while (caseChoisie < 0 || caseChoisie > 5)
+    while (caseChoisie < 0 || caseChoisie > 11)
     {
-        printf("%s, choisissez une case (entre 0 et 5): ", joueur[j]);
-        scanf("%d", &caseChoisie);
-        printf("\r\n");
-    }
+        if (j == 0)
+        {
+            printf("%s, choisissez une case (entre 0 et 5): ", joueur[j]);
+            scanf("%d", &caseChoisie);
+            printf("\r\n");
+        }
+        else
+        {
+            printf("%s, choisissez une case (entre 6 et 11): ", joueur[j]);
+            scanf("%d", &caseChoisie);
+            printf("\r\n");
+        }
 
-    caseChoisie = caseChoisie + 6 * j;
+        /* VONT ÊTRE REMPLACÉ PAR AUTORIZEMOUV*/
+        if (j == 0 && (caseChoisie < 0 || caseChoisie > 5))
+        {
+            printf("**/!\\ Case invalide !**\r\n");
+        }
+        else if (j == 1 && (caseChoisie < 6 || caseChoisie > 11))
+        {
+            printf("**/!\\ Case invalide !**\r\n");
+        }
+
+        if (plateau[caseChoisie] == 0)
+        {
+            printf("Case vide !\r\n");
+        }
+    }
 
     int graines = plateau[caseChoisie];
     plateau[caseChoisie] = 0;
 
+    int caseInitiale = caseChoisie;
+
     while (graines > 0)
     {
-        if (caseChoisie < 6)
+        if (caseChoisie <= 0)
         {
-            caseChoisie = (caseChoisie - 1);
+            caseChoisie = 6;
+        }
+        else if (caseChoisie >= 11)
+        {
+            caseChoisie = 5;
+        }
+        else if (caseChoisie < 6)
+        {
+            caseChoisie--;
         }
         else if (caseChoisie > 5)
         {
-            caseChoisie = (caseChoisie + 1);
+            caseChoisie++;
+        }
+
+        if (caseChoisie == caseInitiale)
+        {
+            if (caseChoisie == 0)
+            {
+                caseChoisie = 6;
+            }
+            else if (caseChoisie == 11)
+            {
+                caseChoisie = 5;
+            }
+            else if (caseChoisie < 6)
+            {
+                caseChoisie--;
+            }
+            else if (caseChoisie > 5)
+            {
+                caseChoisie++;
+            }
         }
 
         plateau[caseChoisie]++;
         graines--;
+    }
+
+    // si la case d'arrivée est adverse et 1 < nbGraine < 4, on prend les graines
+    if (aFiniTerrainAdverse(j, plateau, caseChoisie) && plateau[caseChoisie] > 1 && plateau[caseChoisie] < 4)
+    {
+        // printf("Vous avez pris les graines de la case %d !\r\n", caseChoisie);
+        // plateau[caseChoisie] = 0;
+        printf("Je peu prendre la case %d", caseChoisie);
     }
 }
 
@@ -107,11 +170,44 @@ void afficherPlateau(int *plateau, char **joueur)
         }
     }
 
-    printf("|\r\n\r\n");
+    printf("|\r\n");
+    printf("     ––––––––––––––––––––––––––––––– \r\n");
+    printf("Case: –06– –07– –08– –09– –10– –11– \r\n\r\n");
 }
 
 int hasWin()
 {
     // true (!= 0) si un des joueurs a gagné
+    return 0;
+}
+
+int aFiniTerrainAdverse(int j, int *plateau, int caseChoisie)
+{
+    int tableauJ1[] = {0, 1, 2, 3, 4, 5};
+    int tableauJ2[] = {6, 7, 8, 9, 10, 11};
+
+    if (j == 0)
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            if (plateau[tableauJ2[i]] == caseChoisie)
+            {
+                printf("terrain adverse");
+                return 1;
+            }
+        }
+    }
+    else
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            if (plateau[tableauJ1[i]] == caseChoisie)
+            {
+                printf("terrain adverse");
+                return 1;
+            }
+        }
+    }
+
     return 0;
 }
