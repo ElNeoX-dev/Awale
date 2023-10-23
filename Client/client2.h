aa08c3aeb3ff524ae3ac99565a5724aca98f0ac5
ElNeoX-dev <61790793+ElNeoX-dev@users.noreply.github.com>
2023-10-23 08:43:13 +0200

    merge

--- Client/client2.h
+++ Client/client2.h
@@ -29,6 +29,13 @@ typedef struct in_addr IN_ADDR;
 
 #define CRLF     "\r\n"
 #define PORT     1977
+#define VERT "\e[0;32m"
+#define ROUGE "\e[0;31m"
+#define JAUNE "\e[1;33m"
+#define VIOLET "\e[0;35m"
+#define BLEU "\e[0;34m"
+#define BOLD "\033[1m"
+#define RESET "\033[0m"
 
 #define BUF_SIZE 1024
 
